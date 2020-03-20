a6c8fc0c1c8c924dae6fb5b563590af3c7cd87d9
Boyu1997 <jiangby97@gmail.com>
2020-03-20 16:29:31 -0700

    fix: model evaluation

--- evaluation/model_eval.py
+++ evaluation/model_eval.py
@@ -1,14 +1,35 @@
 import torch
 import json
+import argparse
 
+import sys
+sys.path.append("..")
 from model.densenet import DenseNet
-from model.densenet2 import DenseNet2
 from data import load_data
 
-def model_test(model, loader, device):
+class Args():
+    def __init__(self, args, device):
+        self.cross_block_rate = args['cross_block_rate']
+        self.end_block_reduction_rate = args['end_block_reduction_rate']
+        self.stages = args['stages']
+        self.growth = args['growth']
+        self.group_1x1 = args['group_1x1']
+        self.group_3x3 = args['group_3x3']
+        self.bottleneck = args['bottleneck']
+        self.device = device
+
+
+
+def load_configuration(directory, config_file_name, device):
+    f = open('{:s}/{:s}'.format(directory, config_file_name),'r')
+    config = json.load(f)
+    f.close()
+    args = Args(config, device)
+    return args
+
+def test_model(model, loader, device):
     correct = 0
     total = 0
-
     model.to(device)
     with torch.no_grad():
         for data in loader:
@@ -23,10 +44,10 @@ def model_test(model, loader, device):
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
-
     return correct / total
 
-def get_perimeter_count(model):
+
+def get_parameter_count(model):
     perimeter_count = 0
     for layer_perimeter in list(model.parameters()):
         perimeter_count_in_layer = 1
@@ -35,54 +56,68 @@ def get_perimeter_count(model):
         perimeter_count += perimeter_count_in_layer
     return perimeter_count
 
-class Args():
-    def __init__(self, args):
-        self.stages = args['stages']
-        self.growth = args['growth']
-        self.group_1x1 = args['group_1x1']
-        self.group_3x3 = args['group_3x3']
-        self.bottleneck = args['bottleneck']
+def model_eval(directory, config_file_name, state_dict_file_name, save_file_name):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print ('Using device:', device)
+
+    # load the model
+    model_args = load_configuration(directory, config_file_name, device)
+    model = DenseNet(model_args)
+    model_state_dict = torch.load('{:s}/{:s}'.format(directory, state_dict_file_name), map_location=device)
+    model.load_state_dict(model_state_dict)
+
+    # get performance
+    trainloader, validateloader, testloader, classes = load_data(args.bsize)
+    eval_result = {}
+
+    eval_result['parameter_size'] = get_parameter_count(model)
+    # eval_result['train_accuracy'] = test_model(model, trainloader, device)
+    # eval_result['validate_accuracy'] = test_model(model, validateloader, device)
+    # eval_result['test_accuracy'] = test_model(model, testloader, device)
 
 
-args_d = {'stages': [14, 14, 14],
-          'growth': [8, 16, 32],
-          'group_1x1': 4,
-          'group_3x3': 4,
-          'bottleneck': 4}
-args = Args(args_d)
+    print (eval_result)
+    f = open('{:s}/{:s}'.format(directory, save_file_name),'w')
+    f.write(json.dumps(eval_result))
+    f.close()
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print ('Using device:', device)
 
-densenet = DenseNet(args)   # baseline model
-densenet2 = DenseNet2(args)   # experiment model
+if __name__ == '__main__':
 
-trainloader, validateloader, testloader, classes = load_data(args.bsize)
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--bsize',
+        type=int,
+        default=128,
+        help='model predict batch size'
+    )
+    parser.add_argument(
+        '--directory',
+        type=str,
+        default='../save/default',
+        help='path to the save directory'
+    )
+    parser.add_argument(
+        '--config_file_name',
+        type=str,
+        default='config.json',
+        help='name of model configuration file'
+    )
+    parser.add_argument(
+        '--state_dict_file_name',
+        type=str,
+        default='train/epoch-60.pth',
+        help='name of model configuration file'
+    )
+    parser.add_argument(
+        '--save_file_name',
+        type=str,
+        default='eval.json',
+        help='name of file to save evaluation result'
+    )
+    args, _ = parser.parse_known_args()
 
-best_densenet_epoch = 60
-best_densenet = DenseNet(args)
-best_densenet_dict = torch.load('./save/densenet/train/epoch-{:02d}.pth'.format(best_densenet_epoch), map_location=device)
-best_densenet.load_state_dict(best_densenet_dict)
-
-best_densenet2_epoch = 100
-best_densenet2 = DenseNet2(args)
-best_densenet2_dict = torch.load('./save/densenet2/train/epoch-{:02d}.pth'.format(best_densenet2_epoch), map_location=device)
-best_densenet2.load_state_dict(best_densenet2_dict)
-
-print ('Perimeters in DenseNet: {:.2f}k'.format(get_perimeter_count(best_densenet)*1e-3))
-print ('Perimeters in DenseNet2: {:.2f}k'.format(get_perimeter_count(best_densenet2)*1e-3))
-
-eval_result = {'densenet': {}, 'densenet2': {}}
-
-eval_result['densenet']['train_accuracy'] = model_test(best_densenet, trainloader, device)
-eval_result['densenet']['validate_accuracy'] = model_test(best_densenet, validateloader, device)
-eval_result['densenet']['test_accuracy'] = model_test(best_densenet, testloader, device)
-
-eval_result['densenet2']['train_accuracy'] = model_test(best_densenet2, trainloader, device)
-eval_result['densenet2']['validate_accuracy'] = model_test(best_densenet2, validateloader, device)
-eval_result['densenet2']['test_accuracy'] = model_test(best_densenet2, testloader, device)
-
-print (eval_result)
-f = open('eval.json','w')
-f.write(json.dumps(eval_result))
-f.close()
+    model_eval(directory=args.directory,
+               config_file_name=args.config_file_name,
+               state_dict_file_name=args.state_dict_file_name,
+               save_file_name=args.save_file_name)
