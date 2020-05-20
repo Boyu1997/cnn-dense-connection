a4eaee2fd8c58642de63d8871e6bf8dec7488403
Boyu1997 <jiangby97@gmail.com>
2020-05-20 17:36:03 -0400

    refactor(model): model config from dict

--- main.py
+++ main.py
@@ -3,15 +3,68 @@ import argparse
 import numpy as np
 import torch
 
-import torch.nn as nn   # nn for parallel
-
 # load model
 from model.densenet import DenseNet
 from data import load_data
 from traning.train import train_model
 
-def main(args):
+class Args():
+    def __init__(self, args_dict):
+
+        # dense connection rate
+        self.cross_block_rate = args_dict['cross_block_rate'] if 'cross_block_rate' in args_dict else 0.5   # default = 0.5; range = [0, 1]
+        self.end_block_reduction_rate = args_dict['end_block_reduction_rate'] if 'end_block_reduction_rate' in args_dict else 0.5   # default = 0.5; range = [0, 1]
+
+        # model hyperparameter
+        self.stages = list(map(int, args_dict['stages'].split('-'))) if 'stages' in args_dict else [3,3,3]
+        self.growth = list(map(int, args_dict['growth'].split('-'))) if 'growth' in args_dict else [4,8,12]
+        self.group_1x1 = args_dict['group_1x1'] if 'group_1x1' in args_dict else 4
+        self.group_3x3 = args_dict['group_3x3'] if 'group_3x3' in args_dict else 4
+        self.bottleneck = args_dict['bottleneck'] if 'bottleneck' in args_dict else 4
+
+        self.lr = args_dict['lr'] if 'lr' in args_dict else 1e-2
+        self.ep = args_dict['ep'] if 'ep' in args_dict else 3
+        self.optimizer = args_dict['optimizer'] if 'optimizer' in args_dict else 'adam'   # default = 'adam'; options = {'sgd', 'adam'}
+        self.scheduler = args_dict['scheduler'] if 'scheduler' in args_dict else 'cos'   # default = 'cos'; options = {'clr', 'exp', 'mlr', 'cos'}
+
+        # training batch
+        self.bsize = args_dict['bsize'] if 'bsize' in args_dict else 512
+        self.one_batch = args_dict['one_batch'] if 'one_batch' in args_dict else False
+
+        # folder name for saving result (default: default)
+        self.save_folder = args_dict['save_folder'] if 'save_folder' in args_dict else 'default'
+
+        # data validation
+        if len(self.stages) != len(self.growth):
+            raise RunTimeError("Stages and growth must have the same length")
+
+    def __str__(self):
+        print_str = ("{{cross_block_rate={:.1f},".format(self.cross_block_rate) +
+            "end_block_reduction_rate={:.1f},".format(self.end_block_reduction_rate) +
+            "stages=[{:s}],".format(','.join(map(str, self.stages))) +
+            "growth=[{:s}],".format(','.join(map(str, self.growth))) +
+            "group_1x1={:d},".format(self.group_1x1) +
+            "group_3x3={:d},".format(self.group_3x3) +
+            "bottleneck={:d},".format(self.bottleneck) +
+            "lr={:.3f},".format(self.lr) +
+            "ep={:d},".format(self.ep) +
+            "bottleneck={:d},".format(self.bottleneck) +
+            "optimizer=\'{:s}\',".format(self.optimizer) +
+            "scheduler=\'{:s}\',".format(self.scheduler) +
+            "bsize={:d},".format(self.bsize) +
+            "one_batch=\'{:s}\',".format(str(self.one_batch)) +
+            "save_folder=\'{:s}\'}}".format(self.save_folder))
+        return print_str
+
+
+def main(args_dict):
+    if isinstance(args_dict, dict):
+        args = Args(args_dict)
+    else:
+        print ("No model configuration, using default..")
+        args = Args(dict())
     print (args)
+
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     args.device = device
     print ('Using device:', device)
@@ -19,136 +72,10 @@ def main(args):
     # load data
     trainloader, validateloader, testloader, classes = load_data(args.bsize)
 
-    # config for parallel gpu
-    if args.parallel:
-      densenet = nn.DataParallel(densenet)
-      args.bsize *= args.n_gpu
-
     # model training
     densenet = DenseNet(args)
     train_model(densenet, trainloader, validateloader, device, args)
 
 
-
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-
-    # cross-block enable
-    # 0 for disable cross-block connection
-    # rate between 0 and 1
-    parser.add_argument(
-        '--cross_block_rate',
-        type=float,
-        default=1,
-        help='enable cross-block connection (float range 0 to 1, default: 0.5)'
-    )
-    parser.add_argument('--end_block_reduction_rate',
-        type=float,
-        default=0.5,
-        help='transition reduction rate at the end of each dense block (default: 0.5)'
-    )
-
-    # model config
-    parser.add_argument(
-        '--stages',
-        type=str,
-        default='10-10-10',
-        help='stages (default: 10-10-10)'
-    )
-    parser.add_argument(
-        '--growth',
-        type=str,
-        default='8-16-32',
-        help='growth rates (for each stage, default: 8-16-32)'
-    )
-    parser.add_argument(
-        '--group_1x1',
-        type=int,
-        default=4,
-        help='1x1 group conv (default: 4)'
-    )
-    parser.add_argument(
-        '--group_3x3',
-        type=int,
-        default=4,
-        help='3x3 group conv (default: 4)'
-    )
-    parser.add_argument(
-        '--bottleneck',
-        type=int,
-        default=4,
-        help='bottleneck (default: 4)'
-    )
-
-    # training config
-    parser.add_argument(
-        '--optimizer',
-        type=str,
-        default='sgd',
-        help='optimizer (default: sgd; options: sgd, adam)'
-    )
-    parser.add_argument(
-        '--lr',
-        type=float,
-        default=1e-1,
-        help='learning rate (default: 0.1)'
-    )
-    parser.add_argument(
-        '--scheduler',
-        type=str,
-        default='cos',
-        help='learning rate scheduler (default: cos; options: clr, exp, mlr, cos)'
-    )
-    parser.add_argument(
-        '--ep',
-        type=int,
-        default=300,
-        help='number of epochs (default: 300)'
-    )
-    parser.add_argument(
-        '--bsize',
-        type=int,
-        default=512,
-        help='batch size (default: 512)'
-    )
-    parser.add_argument(
-        '--one_batch',   # one batch option for local testing on cpu
-        dest='one_batch',
-        action='store_true',
-        default=False,
-        help='only train for the first batch'
-    )
-
-    # config for parallel gpu training
-    parser.add_argument(
-        '--parallel',
-        dest='parallel',
-        action='store_true',
-        default=False,
-        help='use parallel gpu training'
-    )
-    parser.add_argument(
-        '--n_gpu',
-        type=int,
-        default=4,
-        help='number of gpu if parallel (default: 4)'
-    )
-
-    # file path
-    parser.add_argument(
-        '--save_folder',
-        type=str,
-        default='default',
-        help='folder name for saving result (default: default)'
-    )
-
-    # parse args
-    args, unparsed = parser.parse_known_args()
-
-    # arg processing
-    args.stages = list(map(int, args.stages.split('-')))
-    args.growth = list(map(int, args.growth.split('-')))
-
-    if len(unparsed) != 0:
-        raise SystemExit('Unknown argument: {}'.format(unparsed))
     main(args)
