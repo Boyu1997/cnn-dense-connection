9b03fc18ee953a436993239b60f2aa7f0f6af7cc
Boyu1997 <jiangby97@gmail.com>
2020-05-20 17:36:49 -0400

    feat(gcp): model config in batch

--- gcp/batch.py
+++ gcp/batch.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import json
 from dotenv import load_dotenv
 from googleapiclient import discovery
@@ -11,14 +12,26 @@ from helper import get_bucket, delete_instance
 load_dotenv()
 PROJECT = os.getenv('PROJECT')
 ZONE = os.getenv('ZONE')
-NAME = os.getenv('NAME')
-BUCKET = os.getenv('BUCKET')
+VM_NAME = os.getenv('VM_NAME')
+BUCKET_NAME = os.getenv('BUCKET_NAME')
+MODEL_NAME = os.getenv('MODEL_NAME')
+MODEL_CONFIG = os.getenv('MODEL_CONFIG')
+model_config = json.loads(MODEL_CONFIG)
+
+
+# import model training scripts
+sys.path.append('..')
+from main import main, Args
+
+
+# train model
+main(model_config)
 
 
 # save data to cloud storage
 client = storage.Client()
-bucket = get_bucket(client, BUCKET)
-blob = bucket.blob('{:s}/test.json'.format(NAME))
+bucket = get_bucket(client, BUCKET_NAME)
+blob = bucket.blob('{:s}/test.json'.format(MODEL_NAME))
 
 data = {'test': 'success'}
 blob.upload_from_string(json.dumps(data))
@@ -26,4 +39,4 @@ blob.upload_from_string(json.dumps(data))
 
 # delete vm instance after experiments
 compute = discovery.build('compute', 'v1')
-delete_instance(compute, PROJECT, ZONE, NAME)
+delete_instance(compute, PROJECT, ZONE, VM_NAME)
--- gcp/master.py
+++ gcp/master.py
@@ -1,4 +1,5 @@
 import os
+import json
 from dotenv import load_dotenv
 from googleapiclient import discovery
 from google.cloud import storage
@@ -8,30 +9,68 @@ from helper import get_bucket, create_instance
 load_dotenv()
 PROJECT = os.getenv('PROJECT')
 ZONE = os.getenv('ZONE')
-BUCKET = os.getenv('BUCKET')
+BUCKET_NAME = os.getenv('BUCKET_NAME')
 
 client = storage.Client()
-bucket = get_bucket(client, BUCKET)
+bucket = get_bucket(client, BUCKET_NAME)
 print ("Obtained storage bucket \'{:s}\'".format(bucket.name))
 
 
 compute = discovery.build('compute', 'v1')
 
-names = ['batch-1', 'batch-2']
 
-for name in names:
+models = [
+    {
+        'vm_name': 'batch-1',
+        'model_name': 'cbr=0',
+        'model_config': {
+            'cross_block_rate': 0,
+            'end_block_reduction_rate': 0.5,
+            'ep': 3
+        }
+    },
+    {
+        'vm_name': 'batch-2',
+        'model_name': 'cbr=0.5',
+        'model_config': {
+            'cross_block_rate': 0.5,
+            'end_block_reduction_rate': 0.5,
+            'ep': 3
+        }
+    }
+]
+
+
+for model in models:
+
+    print (json.dumps(model['model_config']))
+
+    env_string = ("PROJECT=\'{:s}\'\n".format(PROJECT) +
+        "ZONE=\'{:s}\'\n".format(ZONE) +
+        "VM_NAME=\'{:s}\'\n".format(model['vm_name']) +
+        "BUCKET_NAME=\'{:s}\'\n".format(bucket.name) +
+        "MODEL_NAME=\'{:s}\'\n".format(model['model_name']) +
+        "MODEL_CONFIG=\'{:s}\'".format(json.dumps(model['model_config']).replace('\"', '\\\"')))
+
+    print (env_string)
+
+
+    print ("echo \"{:s}\" > .env\n".format(env_string))
+
+
+    break
 
     startup_script = ("#! /bin/bash\n" +
         "sudo apt-get update\n" +
         "sudo apt-get install -y git\n" +
         "git clone https://github.com/Boyu1997/cnn-dense-connection\n" +
         "cd /cnn-dense-connection/gcp\n" +
-        "echo \"PROJECT=\'{:s}\'\nZONE=\'{:s}\'\nNAME=\'{:s}\'\nBUCKET=\'{:s}\'\" > .env\n".format(PROJECT, ZONE, name, bucket.name) +
+        "echo \"{:s}\" > .env\n".format(env_string) +
         "pip3 install -r requirements.txt\n" +
         "python3 batch.py")
 
-    response = create_instance(compute, PROJECT, ZONE, name, startup_script)
+    response = create_instance(compute, PROJECT, ZONE, model['vm_name'], startup_script)
     if response == 'success':
-        print ("Successfully created vm instance \'{:s}\'".format(name))
+        print ("Successfully created vm instance \'{:s}\'".format(model['vm_name']))
     else:
-        raise RuntimeError("Unable to create vm instance \'{:s}\'".format(name))
+        raise RuntimeError("Unable to create vm instance \'{:s}\'".format(model['vm_name']))
